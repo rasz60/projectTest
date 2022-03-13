47b80df0474b37dda7eda090e9d5457671a48594
FSUNNY <96336832+F-SUNNY@users.noreply.github.com>
2022-03-13 21:30:32 +0900

    Delete whereRU directory

--- whereRU/src/main/java/com/whereru/main/BoardDAO/InterfaceBoardDAO.java
+++
@@ -1,6 +0,0 @@
-package com.whereru.main.BoardDAO;
-
-public interface InterfaceBoardDAO {
-	public void test();
-
-}
--- whereRU/src/main/java/com/whereru/main/BoardDTO/MainDTO.java
+++
@@ -1,9 +0,0 @@
-package com.whereru.main.BoardDTO;
-
-public class MainDTO {
-	private String nickname;
-	private String title;
-	private String content;
-	private String location;
-	private String img;
-}
