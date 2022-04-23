d5fbbcb26d3fd448cc424a44a71b6709872a18d2
rasz60 <96821067+rasz60@users.noreply.github.com>
2022-04-23 21:31:41 +0900

    Update BoardIDao.java

--- project_init/src/main/java/com/project/init/dao/BoardIDao.java
+++ project_init/src/main/java/com/project/init/dao/BoardIDao.java
@@ -6,13 +6,18 @@ import com.project.init.dto.NoticeBoardDto;
 
 public interface BoardIDao {
 	
+	// 전체 공지사항 게시글 불러오기
 	ArrayList<NoticeBoardDto> getBoardList();
 	
+	// 공지사항 게시판 글 작성
 	public void write(String bName, String bTitle, String bContent);
 	
+	// 공지사항 게시글 조회
 	NoticeBoardDto contentView(String bid);
-
+	
+	// 공지사항 게시글 수정
 	void noticeModify(NoticeBoardDto dto);
 	
+	// 공지사항 게시글 삭제
 	void noticeDelete(int bId);
 }
