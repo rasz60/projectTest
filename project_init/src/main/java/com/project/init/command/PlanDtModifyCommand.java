bf9813a71a990ed9378bb353a8c7892a5cd40cf4
rasz60 <96821067+rasz60@users.noreply.github.com>
2022-04-23 21:47:44 +0900

    Update PlanDtModifyCommand.java

--- project_init/src/main/java/com/project/init/command/PlanDtModifyCommand.java
+++ project_init/src/main/java/com/project/init/command/PlanDtModifyCommand.java
@@ -34,19 +34,19 @@ public class PlanDtModifyCommand implements ICommand {
 		User user = (User)authentication.getPrincipal();
 		String uId = user.getUsername();
 		
-		// ó���� query�� �����ؼ� ���� list ����
+		// 상세 일정(PlanDt) 수정된 내용으로 구분해서 담을 배열 선언
 		ArrayList<PlanDtDto> deleteDtDtos = new ArrayList<PlanDtDto>();
 		ArrayList<PlanDtDto> insertDtDtos = new ArrayList<PlanDtDto>();
 		ArrayList<PlanDtDto> updateDtDtos = new ArrayList<PlanDtDto>();
 		
-		// deleteDtNum : ������ ������ �ϳ��� ���� ��
+		// 1. deleteDtNum : 상세 일정(PlanDt) 수정할 때 기존 db에 저정된 일정이 삭제된 경우
 		if(! request.getParameter("deleteDtNum").equals("") ) {
 			logger.info("detailModifyDo deleteNum is exist");
 			
-			// parameter�� �Ѿ�� deleteDtNum�� '/'�� �����Ͽ� �迭�� ����
+			// 삭제된 상세 일정 번호(PlanDtNum)을 배열에 저장
 			String[] deleteDtNums = request.getParameter("deleteDtNum").split("/");
 			
-			// deleteDtNums �迭�� ��� planDtNum���� dto�� ���� list�� add
+			// 삭제된 상세 일정 번호(PlanDtNum)와 아이디를 추가한 dto를 deleteDtDtos add
 			for ( int i = 0; i < deleteDtNums.length; i++ ) {
 				
 				PlanDtDto dto = new PlanDtDto();
@@ -57,21 +57,21 @@ public class PlanDtModifyCommand implements ICommand {
 			}
 			logger.info("detailModifyDo result1: deleteDtDtos.size() ? " + deleteDtDtos.size());
 			
+		// 삭제된 일정이 없으면 처리하지 않음
 		} else {
 			logger.info("detailModifyDo deleteNum is null");
 		}
 		
-		// ���� ���������� �Ѿ�� parameter parsing�ؼ� Dto��ü list ����
+		// 2. 상세 일정(PlanDt) 수정 후 submit된 전체 일정 정보를 불러옴
 		int planNum = Integer.parseInt(request.getParameter("planNum"));
 		ArrayList<PlanDtDto> dtos = (ArrayList)Constant.planDtDtoParser(planNum, uId, request);
 
-		// Dto�� ���� �ϳ��� add
 		for ( int i = 0 ; i < dtos.size(); i++ ) {
-			// planDtNum == 0 : ���� �߰��� �� �������� insert
+			// planDtNum == 0 : 상세 일정 수정하면서 새로 추가된 일정은 insertDtDtos add
 			if ( dtos.get(i).getPlanDtNum() == 0 ) {
 				insertDtDtos.add(dtos.get(i));
 				
-			// planDtNum != 0 : ������ �ִ� �� �������� update
+			// planDtNum != 0 : 상세 일정 수정하면서 기존에 있던 일정은 updateDtDtos add
 			} else {
 				updateDtDtos.add(dtos.get(i));
 			}
