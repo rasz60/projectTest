b4148899d63bd6d2aff18071e9b21014d7fa7ed8
rasz60 <96821067+rasz60@users.noreply.github.com>
2022-04-23 21:25:37 +0900

    Update PostController.java

--- project_init/src/main/java/com/project/init/controller/PostController.java
+++ project_init/src/main/java/com/project/init/controller/PostController.java
@@ -55,7 +55,7 @@ public class PostController {
 		Constant.udao = udao;
 	}
 
-
+	// 포스트 등록 페이지로 접근 시
 	@RequestMapping("posting")
 	public String posting(String planNum, Model model) {
 		logger.info("posting(" + planNum + ") in >>>");
@@ -64,12 +64,13 @@ public class PostController {
 		User user = (User)authentication.getPrincipal();
 		String uId = user.getUsername();
 		
-		
+		// 선택한 일정(PlanMst) 정보 가져옴
 		PlanMstDto result1= pdao.selectPlanMst(planNum, uId);
 		model.addAttribute("plan1", result1);
 		
 		logger.info("posting("+ planNum +") result1.isEmpty() ? " + result1.getDateCount());
 		
+		// 선택한 상세 일정(PlanDt) 정보 가져옴
 		ArrayList<PlanDtDto> result2= pdao.selectPlanDt(planNum, uId);
 		model.addAttribute("plan2", result2);
 		
@@ -80,13 +81,12 @@ public class PostController {
 		return "post/addPost";
 	}
 	
-	
+	// post 작성 완료 시
 	@RequestMapping(value = "uploadMulti", method = { RequestMethod.POST })
 	public String uploadMulti(MultipartHttpServletRequest multi, Model model) {
 		logger.info("uploadMulti() in >>>");		
 		
 		comm = new PostWriteCommand();
-		
 		comm.execute(multi, model);
 		
 		logger.info("uploadMulti result : " + multi.getAttribute("result").toString());
@@ -94,6 +94,7 @@ public class PostController {
 		return "redirect:/feed";
 	}
 	
+	// mypost 접근
 	@RequestMapping("mypost")
 	public String mypost(Model model) {
 		logger.info("mypost() in >>>");
@@ -109,7 +110,8 @@ public class PostController {
 		
 		model.addAttribute("planCount", planCount);
 		model.addAttribute("postCount", postCount);		
-
+		
+		// 내 아이디로 등록된 전체 포스트 가져오기
 		ArrayList<PostDto> list = postDao.mylist(uId, model);
 		
 		model.addAttribute("list", list);
@@ -120,6 +122,7 @@ public class PostController {
 		return "post/mypost";
 	}
 	
+	// 포스트 조회시 1개 id당 1번씩 조회수 up
 	@ResponseBody
 	@RequestMapping("addView.do")
 	public String addView(@RequestParam("postNo") String postNo,@RequestParam("email") String email) {
@@ -134,6 +137,7 @@ public class PostController {
 		
 	}
 	
+	// 포스트에 등록된 전체 댓글 불러오기
 	@ResponseBody
 	@RequestMapping(value="getcomments.do")
 	public ArrayList<CommentsDto> getcomments(@RequestParam("postNo") String postNo) {
@@ -146,7 +150,7 @@ public class PostController {
 		return dto;
 	}
 	
-	
+	// 포스트 대댓글 작성 submit
 	@ResponseBody
 	@RequestMapping("addReplyComments.do")
 	public void addReplyComments(@RequestParam("postNo") String postNo,@RequestParam("grp") String grp, @RequestParam("content") String content,@RequestParam("grpl") String grpl,@RequestParam("grps") String grps,@RequestParam("email") String email){
@@ -160,6 +164,7 @@ public class PostController {
 
 	}
 	
+	// 포스트 댓글 삭제 submit
 	@ResponseBody
 	@RequestMapping("deleteReplyComments.do")
 	public void deleteReplyComments(@RequestParam("commentNo") String commentNo){
@@ -168,7 +173,7 @@ public class PostController {
 		postDao.deleteReplyComments(commentNo);		
 	}
 	
-	
+	// 포스트 댓글 작성 submit
 	@ResponseBody
 	@RequestMapping("addcomments.do")
 	public void addcomments(@RequestParam("postNo") String postNo, @RequestParam("content") String content,@RequestParam("grpl") String grpl,@RequestParam("email") String email){
@@ -180,6 +185,7 @@ public class PostController {
 		
 	}
 	
+	// 포스트 조회시 1개 id당 1번씩 좋아요 up
 	@ResponseBody
 	@RequestMapping(value="addLike.do",produces = "application/text; charset=UTF-8")
 	public String addLike(@RequestParam("postNo") String postNo, @RequestParam("email") String email) {
@@ -193,6 +199,7 @@ public class PostController {
 		return result;
 	}
 
+	// 포스트 타이틀 이미지 클릭하여 상세 modal 창으로 접근 시
 	@ResponseBody
 	@RequestMapping("getlist.do")
 	public PostDto getlist(@RequestParam("postNo") String postNo, @RequestParam("email") String email, Model model) {
@@ -206,6 +213,7 @@ public class PostController {
 		return tmp;
 	}
 	
+	// 포스트 수정 페이지로 이동시
 	@RequestMapping("modify")
 	public String modify(HttpServletRequest request, Model model) {
 		logger.info("modify(" + request.getParameter("postNo") + ") in >>>");
@@ -222,16 +230,17 @@ public class PostController {
 		return "post/modifypost";
 	}
 	
+	// 포스트 수정 submit
 	@RequestMapping(value = "modifyExcute.do", method = { RequestMethod.POST })
 	public String modifyExcute(MultipartHttpServletRequest multi, Model model) {
 		System.out.println(multi.getParameter("delDtNum"));
-		
 		
 		comm = new PostModifyDoCommand();
 		comm.execute(multi, model);
 		return "redirect:/post/mypost";
 	}
-
+	
+	// 포스트 삭제 submit
 	@RequestMapping("delete.do")
 	public String delete(HttpServletRequest request,Model model) {
 		String postNo = request.getParameter("postNo");
@@ -239,10 +248,13 @@ public class PostController {
 		return "redirect:/post/mypost";
 	}
 	
+	// post Modal창에서 프로필 이미지를 눌러서 해당 유저의 피드로 접근 시
 	@RequestMapping("otherUser")
 	public String otherPost(HttpServletRequest request, Model model) {
+		// 접근하려는 유저의 nickname
 		String nickName = request.getParameter("nick");
 		
+		// 자기 자신의 정보를 얻어 myinfo로 전달
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User user = (User)authentication.getPrincipal();
 		String uId = user.getUsername();
@@ -251,6 +263,7 @@ public class PostController {
 		model.addAttribute("myinfo", dto);
 		System.out.println(dto.getUserNick());
 		
+		// 자기 자신의 nickname과 같으면 mypost로 redirect
 		if ( nickName.equals(dto.getUserNick()) ) {
 			return "redirect:/mypost";
 		}
@@ -260,6 +273,7 @@ public class PostController {
 		map.put("nick", nickName);
 		map.put("uId", uId);
 		
+		// nickname으로 해당 유저의 정보를 불러옴
 		UserDto otherUser = udao.searchNick(map);
 		int planCount = pdao.countPlanMst(otherUser.getUserEmail());
 		int postCount = postDao.countPost(otherUser.getUserEmail());
@@ -269,12 +283,10 @@ public class PostController {
 		
 		UserDto dto2 = udao.login(otherUser.getUserEmail());
 		
+		// 피드의 주인 유저 아이디로 등록된 모든 포스트 불러오기
 		ArrayList<PostDto> list = postDao.mylist(otherUser.getUserEmail(), model);
-		
 		model.addAttribute("list", list);
 		model.addAttribute("user", dto2);
-		
-		
 		
 		logger.info("mypost() result : post.isEmpty ? " + list.isEmpty());
 		
